bb116cf66ac7b3f4bc27964b6bd1e83df6b9280d
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-06-02 01:31:36 -0600

    Still working through images to pick the correct design

--- src/components/findOutMore/FindoutHeader.js
+++ src/components/findOutMore/FindoutHeader.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 
 export const FindoutHeader = styled.div`
-  background: url(${process.env.PUBLIC_URL}/sample-pics/pic6.jpg) no-repeat top;
+  background: url(${process.env.PUBLIC_URL}/sample-pics/teller1.jpg) no-repeat top;
   background-size: cover;
-  min-height: 500px;
+  min-height: 400px;
   padding: 20px;
   border-bottom: 5px solid orange;
 `
